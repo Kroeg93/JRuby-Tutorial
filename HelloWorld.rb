a34d28da2c9502bfffb427e39719a2da56a72606
Unknown <Tim_Kroeger93@gmx.de>
2018-12-05 11:55:36 +0100

    Added Tutorial#4 - File Reader
    Small fixes, txt file for File Reader Example

--- HelloWorld.rb
+++ HelloWorld.rb
@@ -1,5 +1,9 @@
 require "java"                                              # Use Java classes
 
+java_import "java.io.BufferedReader"
+java_import "java.io.FileReader"
+
+
 # FIRST TUTORIAL "HELLO WORLD"
 #(output, literal)
 
@@ -7,7 +11,7 @@ require "java"                                              # Use Java classes
 stringHello= "Hello World"                                  # StingObject "stringHello"
 stringDate = java.util.Date.new                             # StringObject "stringDate"
 
-puts "FIRST TUTORIAL : HELLO WORLD"
+puts "\nFIRST TUTORIAL : HELLO WORLD"
 puts "#{stringHello.to_s}"                                  #puts ausgabe ; to_s convert to string
 puts "Date := #{stringDate.to_s}"
 
@@ -24,17 +28,31 @@ def factorial(n)
 end
 
 # Printing factorial of 5
-puts "SECOND TUTORIAL : FACTORIAL"
+puts "\nSECOND TUTORIAL : FACTORIAL"
 puts factorial(5).to_i
 
 # THIRD TUTORIAL "PRINTING TABLE"
 # better output (print for text),
-print "THIRD TUTORIAL : PRINT TABLE"
-print "Enter number to print table
+
+puts "\nTHIRD TUTORIAL : PRINT TABLE"
+puts "Enter number to print table
 :>"
 n = gets
 #iterates the given block |i| Syntax upto(limit) {|i| block} -> self
 # for example 5.upto(10) { |i| print i, " "} #=> 5 6 7 8 9 10
 1.upto(3) do |i|
   puts "#{n} * #{i} = #{n.to_i * i.to_i}"
+end
+
+# FOURTH TUTORIAL ""
+
+puts "\nFOURTH TUTORIAL : FILE READER EXAMPLE"
+
+fileReader = FileReader.new "textfile.txt"
+bufferReader = BufferedReader.new fileReader
+str = bufferReader.readLine
+
+while str
+  puts str.to_s
+  str = bufferReader.readLine
 end
