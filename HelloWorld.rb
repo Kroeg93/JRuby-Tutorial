7f401477db0df1d41ec8242d188955a051928668
Unknown <Tim_Kroeger93@gmx.de>
2018-12-05 10:47:58 +0100

    Added Tutorial#1
    Hello World - Tutorial

---
+++ HelloWorld.rb
@@ -0,0 +1,17 @@
+
+
+# FIRST TUTORIAL "HELLO WORLD"
+
+require "java"                                              # Use Java classes
+
+stringHello= "Hello World"                                  # StingObject "stringHello"
+stringDate = java.util.Date.new                             # StringObject "stringDate"
+
+puts "FIRST TUTORIAL : HELLO WORLD"
+puts "#{stringHello.to_s}"                                  #puts ausgabe ; to_s convert to string
+puts "Date := #{stringDate.to_s}"
+
+# SECOND TUTORIAL "CALCULATING FACTORIAL"
+
+# define factorial method
+
