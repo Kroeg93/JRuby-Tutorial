5d7b8b41182df637e9c736ab1b56213ff5e5923a
Unknown <Tim_Kroeger93@gmx.de>
2018-12-05 11:19:30 +0100

    Added Tutorial#2: Factorial
    Easy methods

--- HelloWorld.rb
+++ HelloWorld.rb
@@ -1,8 +1,8 @@
-
+require "java"                                              # Use Java classes
 
 # FIRST TUTORIAL "HELLO WORLD"
+#(output, literal)
 
-require "java"                                              # Use Java classes
 
 stringHello= "Hello World"                                  # StingObject "stringHello"
 stringDate = java.util.Date.new                             # StringObject "stringDate"
@@ -12,6 +12,19 @@ puts "#{stringHello.to_s}"                                  #puts ausgabe ; to_s
 puts "Date := #{stringDate.to_s}"
 
 # SECOND TUTORIAL "CALCULATING FACTORIAL"
-
+# (Methods, recursive)
 # define factorial method
 
+def factorial(n)
+  if n == 0
+    1
+  else
+    n * factorial(n-1)
+  end
+end
+
+# Printing factorial of 5
+puts "SECOND TUTORIAL : FACTORIAL"
+puts factorial(5).to_i
+
+# THIRD TUTORIAL "PRINTING TABLE"
